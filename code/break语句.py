ae773ecaec72fb13cf0ea2804d3f8a54ff0b68c3
jiyiyun <jiyiyun@gmail.com>
2016-11-12 20:40:37 +0800

    python study

---
+++ code/break语句.py
@@ -0,0 +1,5 @@
+while True:
+	name = input('you name\n')
+	if name =="stop": break
+	age = input("you age\n")
+	print("hello",name , '=>' ,int(age) ** 2)
---
+++ code/continue语句.py
@@ -0,0 +1,5 @@
+x = 10
+while x:
+	x= x-1
+	if x %2 !=0: continue
+	print(x ,end=' ')
